06f3bbf05e31f31c936c1c68162d669e59339cd7
Erin Yang <asabust@users.noreply.github.com>
2024-11-01 14:51:44 +0900

    feat: Detect boar collisions with walls. #2

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,40 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
     [Header("基本参数")]
     public float normalSpeed;
 
-    public float dashSpeed;
+    public float chaseSpeed;
     public float currentSpeed;
-    public Vector3 direction;
+    public Vector3 faceDirection;
+
+    [Header("计时器")]
+    public float waitTime;
+
+    public float waitTimer;
+    public bool waiting;
 
     private Rigidbody2D rb;
     protected Animator anim;
+    private PhysicsCheck physicsCheck;
 
-    // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        physicsCheck = GetComponent<PhysicsCheck>();
         currentSpeed = normalSpeed;
+        waitTimer = waitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction = new Vector3(-transform.localScale.x, 0, 0);
+        faceDirection = new Vector3(-transform.localScale.x, 0, 0);
+        if ((physicsCheck.touchLeftWall && faceDirection.x < 0) || (physicsCheck.touchRigntWall && faceDirection.x > 0))
+        {
+            waiting = true;
+            anim.SetBool("isWalk", false);
+        }
+
+        Timer();
     }
 
     void FixedUpdate()
     {
-        Move();
+        if (!waiting)
+        {
+            Move();
+        }
     }
 
     public virtual void Move()
     {
-        rb.velocity = new Vector2(normalSpeed * direction.x * Time.deltaTime, rb.velocity.y);
+        rb.velocity = new Vector2(normalSpeed * faceDirection.x * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void TurnAround()
+    {
+        transform.localScale = new Vector3(faceDirection.x, 1, 1);
+    }
+
+    public void Timer()
+    {
+        if (waiting)
+        {
+            waitTimer -= Time.deltaTime;
+            if (waitTimer <= 0)
+            {
+                TurnAround();
+                waiting = false;
+                waitTimer = waitTime;
+            }
+        }
     }
 }
--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -1,13 +1,35 @@
+using System;
 using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    public bool manual;
     public Vector2 bottomOffset;
-    public bool isGrounded;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
+
     public float checkRadius = 0.2f;
     public LayerMask groundLayer;
 
-    // Update is called once per frame
+    [Header("状态")]
+    public bool isGrounded;
+
+    public bool touchLeftWall;
+    public bool touchRigntWall;
+
+
+    private CapsuleCollider2D _capsule;
+
+    private void Start()
+    {
+        _capsule = GetComponent<CapsuleCollider2D>();
+        if (!manual)
+        {
+            rightOffset = new Vector2(_capsule.offset.x + _capsule.size.x / 2, _capsule.offset.y);
+            leftOffset = new Vector2(_capsule.offset.x - _capsule.size.x / 2, _capsule.offset.y);
+        }
+    }
+
     private void Update()
     {
         Check();
@@ -16,11 +38,17 @@ public class PhysicsCheck : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2) transform.localPosition + bottomOffset, checkRadius);
+        Gizmos.DrawWireSphere((Vector2) transform.localPosition + leftOffset, checkRadius);
+        Gizmos.DrawWireSphere((Vector2) transform.localPosition + rightOffset, checkRadius);
     }
 
     private void Check()
     {
         isGrounded =
             Physics2D.OverlapCircle((Vector2) transform.localPosition + bottomOffset, checkRadius, groundLayer);
+        touchLeftWall =
+            Physics2D.OverlapCircle((Vector2) transform.localPosition + leftOffset, checkRadius, groundLayer);
+        touchRigntWall =
+            Physics2D.OverlapCircle((Vector2) transform.localPosition + rightOffset, checkRadius, groundLayer);
     }
 }
